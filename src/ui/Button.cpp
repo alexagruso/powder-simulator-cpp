ec62168c872857cd1604c04b4264ede59cfaff9c
AlexAgruso <agrusoalexander@gmail.com>
2024-03-06 18:15:27 -0600

    basic button logic using event system

--- src/ui/Button.cpp
+++ src/ui/Button.cpp
@@ -1,41 +1,37 @@
 #include "Button.hpp"
 #include "event/Event.hpp"
-#include "util/Overload.hpp"
+#include "util/Overloaded.hpp"
 
+#include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Vector2.hpp>
 
 #include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/Mouse.hpp>
+#include <ios>
 #include <iostream>
 #include <variant>
 
 using namespace Powder;
 
 Button::Button(sf::Vector2u size, sf::Vector2i position)
-    : width{size.x}, height{size.y}, xPosition{position.x}, yPosition{position.y}
+    : size{static_cast<float>(size.x), static_cast<float>(size.y)},
+      position{static_cast<float>(position.x), static_cast<float>(position.y)}
 {
-    this->body = sf::RectangleShape({static_cast<float>(width), static_cast<float>(height)});
-    this->body.setPosition(static_cast<float>(this->xPosition), static_cast<float>(this->yPosition));
+    this->body.setSize(this->size);
+    this->body.setPosition(this->position);
     this->body.setFillColor(sf::Color::Red);
 }
 
 bool Button::contains(sf::Vector2i mousePosition) const
 {
-    bool isContainedOnXAxis = (mousePosition.x >= this->xPosition && mousePosition.x <= this->xPosition + this->width);
-    bool isContainedOnYAxis = (mousePosition.y >= this->yPosition && mousePosition.y <= this->yPosition + this->height);
+    bool isContainedOnXAxis =
+        (mousePosition.x >= this->position.x && mousePosition.x <= this->position.x + this->size.x);
+    bool isContainedOnYAxis =
+        (mousePosition.y >= this->position.y && mousePosition.y <= this->position.y + this->size.y);
 
     return isContainedOnXAxis && isContainedOnYAxis;
-}
-
-bool Button::isActive() const
-{
-    return this->active;
-}
-
-void Button::drawTo(sf::RenderWindow* window) const
-{
-    window->draw(this->body);
 }
 
 void Button::setColor(sf::Color color)
@@ -43,17 +39,17 @@ void Button::setColor(sf::Color color)
     this->body.setFillColor(color);
 }
 
-void Button::earlyUpdate(Event event)
+void Button::handleEvent(const Event& event)
 {
     std::visit(
         Util::Overloaded{
-            [this](MouseEvent value)
+            [this](const MouseEvent& event)
             {
-                if (this->contains({value.x, value.y}))
+                if (this->contains(event.position))
                 {
                     this->setColor(sf::Color::Green);
 
-                    if (value.button)
+                    if (event.query(sf::Mouse::Left, InputStatus::ACTIVE))
                     {
                         this->setColor(sf::Color::Blue);
                     }
@@ -63,7 +59,15 @@ void Button::earlyUpdate(Event event)
                     this->setColor(sf::Color::Red);
                 }
             },
-            [](auto value) {},
+            [](auto _) {},
         },
         event);
+}
+
+void Button::tick(sf::RenderWindow& window)
+{
+    this->body.setSize(this->size);
+    this->body.setPosition(this->position);
+
+    window.draw(this->body);
 }
