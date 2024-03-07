4d553bbb66cf567348572dd559bb22a52948e7ef
AlexAgruso <agrusoalexander@gmail.com>
2024-03-06 18:15:14 -0600

    basic functional event system

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "event/Event.hpp"
 #include "ui/Button.hpp"
+#include "util/Overloaded.hpp"
 
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Event.hpp>
 
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/Mouse.hpp>
+#include <SFML/Window/WindowBase.hpp>
 #include <iostream>
+#include <optional>
 #include <random>
 #include <stack>
 #include <variant>
@@ -16,6 +22,8 @@ const unsigned int WINDOW_HEIGHT = 600;
 const std::string WINDOW_TITLE = "Powder Simulator";
 
 const sf::Color WINDOW_FILL_COLOR = sf::Color::Black;
+
+const unsigned int FRAMERATE_LIMIT = 60;
 
 Powder::Button button{
     {50, 50},
@@ -29,52 +37,111 @@ int main()
         WINDOW_TITLE, sf::Style::Default
     };
 
+    window.setFramerateLimit(FRAMERATE_LIMIT);
+
     std::stack<Powder::Event> events;
+
+    Powder::KeyboardEvent keyboardEvent;
+    Powder::MouseEvent mouseEvent;
 
     while (window.isOpen())
     {
+        using Powder::InputStatus;
+
         sf::Event systemEvent;
 
         while (window.pollEvent(systemEvent))
         {
+            using Powder::InputStatus;
+
             switch (systemEvent.type)
             {
-            case sf::Event::Closed:
-            {
-                window.close();
-                break;
-            }
-            case sf::Event::KeyPressed:
-            {
-                events.push(Powder::KeyboardEvent{systemEvent.key.code});
-                break;
-            }
-            case sf::Event::MouseButtonPressed:
-            {
-                events.push(Powder::MouseEvent{systemEvent.mouseMove.x, systemEvent.mouseMove.y,
-                                               systemEvent.mouseButton.button});
-            }
-            case sf::Event::MouseMoved:
-            {
-                events.push(Powder::MouseEvent{systemEvent.mouseMove.x, systemEvent.mouseMove.y,
-                                               systemEvent.mouseButton.button});
-                break;
-            }
-            default: break;
+                case sf::Event::Closed:
+                {
+                    events.push(Powder::ApplicationExitEvent{});
+                }
+                case sf::Event::KeyPressed:
+                {
+                    keyboardEvent.keys.set(systemEvent.key.code * 3 + static_cast<int>(InputStatus::PRESSED));
+                    keyboardEvent.keys.set(systemEvent.key.code * 3 + static_cast<int>(InputStatus::ACTIVE));
+                    break;
+                }
+                case sf::Event::KeyReleased:
+                {
+                    keyboardEvent.keys.set(systemEvent.key.code * 3 + static_cast<int>(InputStatus::RELEASED));
+                    keyboardEvent.keys.reset(systemEvent.key.code * 3 + static_cast<int>(InputStatus::ACTIVE));
+                    break;
+                }
+                case sf::Event::MouseButtonPressed:
+                {
+                    mouseEvent.buttons.set(systemEvent.key.code * 3 + static_cast<int>(InputStatus::PRESSED));
+                    mouseEvent.buttons.set(systemEvent.key.code * 3 + static_cast<int>(InputStatus::ACTIVE));
+                    break;
+                }
+                case sf::Event::MouseButtonReleased:
+                {
+                    mouseEvent.buttons.set(systemEvent.key.code * 3 + static_cast<int>(InputStatus::RELEASED));
+                    mouseEvent.buttons.reset(systemEvent.key.code * 3 + static_cast<int>(InputStatus::ACTIVE));
+                    break;
+                }
+                case sf::Event::MouseMoved:
+                {
+                    mouseEvent.moving = true;
+                    mouseEvent.position = {systemEvent.mouseMove.x, systemEvent.mouseMove.y};
+                    break;
+                }
+                default: break;
             }
         }
 
-        window.clear(WINDOW_FILL_COLOR);
+        events.push(keyboardEvent);
+        events.push(mouseEvent);
 
         while (!events.empty())
         {
+            using namespace Powder::Util;
+
             const Powder::Event& currentEvent = events.top();
 
-            button.earlyUpdate(events.top());
+            button.handleEvent(currentEvent);
+
+            std::visit(Overloaded{[&window](const Powder::ApplicationExitEvent& _) { window.close(); },
+                                  [&window](const Powder::KeyboardEvent& event)
+                                  {
+                                      if (event.query(sf::Keyboard::Escape, InputStatus::PRESSED))
+                                      {
+                                          window.close();
+                                      }
+                                  },
+                                  [](auto _) {}},
+                       currentEvent);
+
             events.pop();
         }
 
-        button.drawTo(&window);
+        // done with events
+
+        // reset input states
+
+        for (int i = 0; i < sf::Keyboard::KeyCount; i++)
+        {
+            keyboardEvent.keys.reset(i * 3 + static_cast<int>(InputStatus::PRESSED));
+            keyboardEvent.keys.reset(i * 3 + static_cast<int>(InputStatus::RELEASED));
+        }
+
+        for (int i = 0; i < sf::Mouse::ButtonCount; i++)
+        {
+            mouseEvent.buttons.reset(i * 3 + static_cast<int>(InputStatus::PRESSED));
+            mouseEvent.buttons.reset(i * 3 + static_cast<int>(InputStatus::RELEASED));
+        }
+
+        mouseEvent.moving = false;
+
+        // drawing to window
+
+        window.clear(WINDOW_FILL_COLOR);
+
+        button.tick(window);
 
         window.display();
     }
