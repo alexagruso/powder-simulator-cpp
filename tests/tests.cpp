d49dd20b18ed07f1e3085687437bcef053cecf1a
AlexAgruso <agrusoalexander@gmail.com>
2024-04-24 00:44:43 -0500

    move project main.cpp and add board test cases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,23 +1,24 @@
+#include "physics/Element.hpp"
+#include "physics/Particle.hpp"
+#include "ui/Board.hpp"
+
 #include <catch2/catch_test_macros.hpp>
 
-uint factorial(uint value)
+TEST_CASE("Board Physics")
 {
-    if (value <= 1)
-        return 1;
+    Powder::Board board{
+        {5, 5}
+    };
 
-    return value * factorial(value - 1);
-}
+    for (int i = 0; i < 5; i++)
+    {
+        board.set({i, i}, Powder::Physics::Particle{Powder::Physics::Fire{}});
+        REQUIRE(board.at({i, i}));
+    }
 
-TEST_CASE("Test Success")
-{
-    REQUIRE(factorial(0) == 1);
-    REQUIRE(factorial(1) == 1);
-    REQUIRE(factorial(2) == 2);
-    REQUIRE(factorial(3) == 6);
-    REQUIRE(factorial(4) == 24);
-}
-
-TEST_CASE("Test Fail")
-{
-    REQUIRE(factorial(5) == 100);
+    for (int i = 0; i < 5; i++)
+    {
+        board.set({i, i}, std::nullopt);
+        REQUIRE(!board.at({i, i}));
+    }
 }
